68a27d67341d3a9025d696cbd3ba2b2f7eb099c7
Orima574 <boblion574@outlook.fr>
2024-08-05 17:19:22 +0200

    error solved

--- src/controller/budget.controller.ts
+++ src/controller/budget.controller.ts
@@ -1,18 +1,14 @@
 import {createBudget} from "../mongo/repository/budgetRepository";
 
 export async function addBudget(req,res){
-    if(!req.user){
-        return res.status(401).send({success: false, message: "User not logged in"})
+    const {name} = req.body
+    if(!name){
+        return res.status(403).send({success: false, message:"No data to process"})
+    }
+    const budgetCreate = await createBudget(name,req.user.id);
+    if(budgetCreate !== null){
+        return res.send({success: true, message: "Add new budget"})
     } else {
-        const {name} = req.body
-        if(!name){
-            return res.status(403).send({success: false, message:"No data to process"})
-        }
-        const budgetCreate = await createBudget(name,req.user.id);
-        if(budgetCreate !== null){
-            return res.send({success: true, message: "Add new budget"})
-        } else {
-            return res.send({success: false, message: "Not add new budget"})
-        }
+        return res.send({success: false, message: "Not add new budget"})
     }
 }
